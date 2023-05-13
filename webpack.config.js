db6ade177e785e2987581785c27082f525311df1
Artem Lobovskii <lobovskiy@yandex.ru>
2023-05-13 11:21:28 +0300

    refactor: change order of imports in the webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
+const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
-const path = require('path');
 
 module.exports = {
   mode: 'development',
